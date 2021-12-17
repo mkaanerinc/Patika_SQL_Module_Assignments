14af1371a0f45256d3c66fcc5f9f37c40041362a
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2021-12-17 18:57:22 +0300

    Add files via upload

---
+++ Patika_SQL_Assignment_12.sql
@@ -0,0 +1,18 @@
+
+SELECT COUNT(film.title)
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+SELECT COUNT(title) FROM film WHERE rental_rate=(
+SELECT MAX(rental_rate) FROM film
+);
+
+SELECT title FROM film WHERE rental_rate=(
+SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (
+SELECT MIN(replacement_cost) FROM film)
+
+SELECT customer_id
+FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(customer_id) DESC
